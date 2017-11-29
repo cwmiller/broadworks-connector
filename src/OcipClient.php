8286cb5756a0c58cb2999a0177bfe1db070de9cc
Chase Miller <chase@miller.sh>
2017-11-28 22:03:15 -0500

    Couple changes
    Login can now be called instead of waiting until the first request is made.
    Details about the logged in user are now stored after login for use.
    Added support for the echo feature.

--- src/OcipClient.php
+++ src/OcipClient.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace CWM\BroadWorksConnector;
 
 use CWM\BroadWorksConnector\Ocip\OcipResponseException;
@@ -27,6 +28,9 @@ class OcipClient
     /** @var bool */
     private $loggedIn = false;
 
+    /** @var array|null */
+    private $userDetails = null;
+
     /**
      * @param string $wsdlUrl
      * @param string $username
@@ -43,53 +47,79 @@ class OcipClient
     /**
      * @param string $command
      * @param array $args
+     * @param string|null $echo A string can be passed to the API that will be returned in the response. Why you would want this, I do not know.
      * @return array
      * @throws \CWM\BroadWorksConnector\OCIP\OcipResponseException
      */
-    public function call($command, array $args = [])
+    public function call($command, array $args = [], $echo = null)
     {
         if (!$this->loggedIn) {
             $this->login();
         }
 
-        return $this->executeCommand($command, $args);
+        return $this->executeCommand($command, $args, $echo);
     }
 
-    private function login()
+    /**
+     * @return bool
+     */
+    public function isLoggedIn()
     {
-        $authResponse = $this->executeCommand('AuthenticationRequest', [
-            'userId' => $this->username
-        ]);
+        return $this->loggedIn;
+    }
 
-        $nonce = $authResponse['nonce'];
+    /**
+     * @return array|null
+     */
+    public function getUserDetails()
+    {
+        return $this->userDetails;
+    }
 
-        $this->executeCommand('LoginRequest14sp4', [
-            'userId' => $this->username,
-            'signedPassword' => md5($nonce . ':' . sha1($this->password))
-        ]);
+    public function login()
+    {
+        if (!$this->loggedIn) {
+            $response = $this->executeCommand('AuthenticationRequest', [
+                'userId' => $this->username
+            ]);
 
-        // It's pretty safe to assume we're logged in at this point, since any authentication failures
-        // would have resulted in an exception.
-        $this->loggedIn = true;
+            if (!isset($response['nonce'])) {
+                throw new OcipResponseException('AuthenticationRequest did not return a nonce.');
+            }
+
+            $response = $this->executeCommand('LoginRequest14sp4', [
+                'userId' => $this->username,
+                'signedPassword' => md5($response['nonce'] . ':' . sha1($this->password))
+            ]);
+
+            // It's pretty safe to assume we're logged in at this point, since any authentication failures
+            // would have resulted in an exception.
+
+            $this->userDetails = $response;
+            unset($this->userDetails['echo']);
+
+            $this->loggedIn = true;
+        }
     }
 
     /**
      * @param string $command
      * @param array $args
+     * @param string|null $echo
      * @return array
      * @throws \CWM\BroadWorksConnector\OCIP\OcipResponseException
      */
-    private function executeCommand($command, array $args = [])
+    private function executeCommand($command, array $args = [], $echo = null)
     {
-        $xml = $this->buildCommandXml($command, $args);
+        $xml = $this->buildCommandXml($command, $args, $echo);
 
-        $rawResponse = $this->soap->processOCIMessage(['in0' => $xml->asXML()]);
+        $response = $this->soap->processOCIMessage(['in0' => $xml->asXML()]);
 
-        if (!isset($rawResponse->processOCIMessageReturn)) {
+        if (!isset($response->processOCIMessageReturn)) {
             throw new OcipResponseException('No processOCIMessageReturn in response.');
         }
 
-        $xmlResponse = @new SimpleXMLElement($rawResponse->processOCIMessageReturn);
+        $xmlResponse = @new SimpleXMLElement($response->processOCIMessageReturn);
         $arrResponse = XmlUtils::toArray($xmlResponse);
 
         if (!isset($arrResponse['command'])) {
@@ -116,17 +146,24 @@ class OcipClient
     /**
      * @param string $command
      * @param array $args
+     * @param string|null $echo
      * @return SimpleXMLElement
      */
-    private function buildCommandXml($command, array $args = []) {
+    private function buildCommandXml($command, array $args = [], $echo = null)
+    {
         $template = <<<EOL
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <BroadsoftDocument protocol="OCI" xmlns="C" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"></BroadsoftDocument>
 EOL;
+
         $xml = @new SimpleXMLElement($template);
         $xml->addChild('sessionId', $this->sessionId, '');
+
         $commandXml = $xml->addChild('command', '', '');
         $commandXml->addAttribute('xmlns:xsi:type', $command);
+        if ($echo !== null) {
+            $commandXml->addAttribute('echo', $echo, '');
+        }
 
         foreach ($args as $k => $v) {
             if (is_bool($v)) {
